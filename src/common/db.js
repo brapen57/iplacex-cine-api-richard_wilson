a4a380a5a0062b8eab6f270522e09990bc528ede
brapen57 <richardalexanderrodriguez@gmail.com>
2024-09-22 23:59:25 -0300

    Update db.js

--- src/common/db.js
+++ src/common/db.js
@@ -1,6 +1,6 @@
 import { MongoClient, ServerApiVersion } from 'mongodb';
 
-const uri = 'mongodb+srv://ev3_express:jnpsskg40eqIbJdf@cluster-express.qsa0d.mongodb.net/?retryWrites=true&w=majority&tls=true&tlsAllowInvalidCertificates=true&appName=cluster-express';
+const uri = 'mongodb+srv://ev3_express:jnpsskg40eqIbJdf@cluster-express.qsa0d.mongodb.net/?retryWrites=true&w=majority&tls=true&tlsAllowInvalidCertificates=true&appName=cluster-express'
 
 const client = new MongoClient(uri, {
     serverApi: {
